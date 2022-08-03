3778fee1e9657d036302a2be4c50e74426aba70e
PawelPohland <pawel.pohland@gmail.com>
2022-08-03 22:49:16 +0200

    Init project structure.

---
+++ mime_types_ext.py
@@ -0,0 +1,35 @@
+# This only contains more common MIME Types
+# https://www.iana.org/assignments/media-types/media-types.xhtml
+
+mime_types_ext = [
+    ("image/gif", ".gif"),
+    ("image/jpeg", ".jpg"),
+    ("image/png", ".png"),
+    ("image/svg+xml", ".svg"),
+    ("image/webp", ".webp"),
+    ("image/bmp", ".bmp"),
+    ("image/x-icon", ".ico"),
+    ("image/tiff", ".tiff"),
+
+    ("text/plain", ".txt"),
+    ("text/csv", ".csv"),
+    ("text/html", ".html"),
+    ("text/css", ".css"),
+    ("text/javascript", ".js"),
+
+    ("audio/ogg", ".ogg"),
+
+    ("video/mp4", ".mp4"),
+    ("video/ogg", ".ogg"),
+
+    ("application/pdf", ".pdf"),
+    ("application/json", ".json"),
+    ("application/rtf", ".rtf"),
+    ("application/zip", ".zip"),
+    ("application/x-zip-compressed", ".zip"),
+    ("application/sql", ".sql"),
+
+    ("font/woff", ".woff"),
+    ("font/ttf", ".ttf"),
+    ("font/otf", ".otf")
+]
